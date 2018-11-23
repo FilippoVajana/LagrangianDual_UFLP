02d579891279f536b8c1be8fe5aef7e8b3ebe361
FilippoVajana <pippovaja@gmail.com>
2018-11-23 15:23:33 +0100

    added parallel run

--- benchmark.py
+++ benchmark.py
@@ -6,135 +6,8 @@ import os
 import time
 import datetime
 import logging as log
-log.basicConfig(level=log.INFO, format='[%(asctime)s] %(levelname)s: %(message)s', datefmt='%H:%M:%S')
+log.basicConfig(level=log.ERROR, format='[%(asctime)s] %(levelname)s: %(message)s', datefmt='%H:%M:%S')
 
-# POJO classes
-class InstanceData():
-    """
-    Represents a problem's instance data
-    """
-    min = 0
-    max = 10
-        
-    def create(self, clients, locations):
-        self.c_num = clients
-        self.l_num = locations
-        self.id = '{row}x{col}_{hash}'.format(row=self.c_num, col=self.l_num, hash=str(rnd.random())[2:6])
-        # generate data        
-        self.U = np.zeros(self.c_num, dtype=int)
-        self.C = np.random.randint(low=self.min, high=self.max+1, size=(self.c_num, self.l_num))        
-        avg_cp = [np.sum(self.C[:,i]) for i in range(self.l_num)]
-        self.F = [np.random.randint(low=avg_cp[i]*0.5, high=avg_cp[i]*1.2+1) for i in range(self.l_num)]
-        return self
-
-    def encode(self):
-        # flatten and byte encode numpy array
-        self.U = self.U.reshape(self.c_num)
-        self.U = self.U.tostring()
-        #print(self.U)
-
-        self.C = np.asarray(self.C)
-        self.C = self.C.reshape(self.c_num * self.l_num)
-        self.C = self.C.tostring()
-        #print(self.C)
-
-        self.F = np.asarray(self.F).reshape(self.l_num)
-        self.F = self.F.tostring()
-        #print(self.F)
-        return self
-    
-    def decode(self):
-        self.U = np.frombuffer(self.U, dtype=int)
-        #print(self.U)
-
-        self.C = np.frombuffer(self.C, dtype=int).reshape((self.c_num, self.l_num))
-        #print(self.C)        
-
-        self.F = np.frombuffer(self.F, dtype=int)
-        #print(self.F)        
-        return self
-
-    def print(self):
-        print("U = ", self.U)        
-        print("F = ", self.F)
-        print("C = ", self.C)
-class Benchmark():
-    DATA_PATH = None
-    def __init__(self):
-        # set data folder
-        path = os.path.join(os.getcwd(), "data")    
-        if not (os.path.exists(path) and os.path.isdir(path)):
-            os.mkdir(path)
-        self.DATA_PATH = path
-
-    def save_instances(self, instances):
-        for i in instances:
-            # serialize instance
-            data = json.encode(i.encode())
-            # create file
-            name = "instance_{}.json".format(i.id)
-            f_path = os.path.join(self.DATA_PATH, name)
-            file = open(f_path, "w+")
-            file.write(data)
-            file.close()
-
-    def load_instances(self):
-        # get files in data folder
-        paths = [os.path.join(self.DATA_PATH, f) for f in os.listdir(self.DATA_PATH) if os.path.isfile(os.path.join(self.DATA_PATH, f))] 
-        # load instances
-        instances = []
-        for p in paths:
-            file = open(p, 'r')
-            data = file.read()
-            file.close()
-            i = json.decode(data, classes=InstanceData).decode()
-            instances.append(i)        
-        return instances
-
-    def build_test_instances(self, shapes=[], count=1):
-        instances = []
-        for (c,l) in shapes:
-            for _ in range(count):
-                time.sleep(.5)
-                instance = InstanceData().create(c,l)
-                instances.append(instance)
-        return instances
-
-    def to_dataframe(self, bnc2_list):
-        '''
-        Converts a list of benchmark results into a pandas DataFrame
-        '''
-        # unfold results
-        unf = [r.unfold() for r in bnc2_list]
-
-        # build data frame
-        cols = list(unf[0].keys())
-        df = pd.DataFrame(data=unf, columns=cols)
-
-        return df
-
-    def merge_dataframe(self, dfs=[]):
-        '''
-        Concats test dataframes
-        '''
-        return pd.concat(dfs)
-
-    def save_dataframe(self, df):
-        '''
-        Saves to a xlsx file the resulting benchmarks dataframe
-        '''
-        dt = datetime.datetime.now().strftime("%d%m_%H%M")
-        name = "benchmark_{}.xlsx".format(dt)
-        path = os.path.join(self.DATA_PATH, name)
-        df.to_excel(path, index=False)
-    
-    def load_dataframe(self, name):
-        '''
-        Loads benchmark dataframe
-        '''
-        path = os.path.join(self.DATA_PATH, name)
-        df = pd.read_excel(path)
-        return df
 
 class InstanceData_v2():
     """
@@ -314,7 +187,6 @@ class Runner:
 
         return df
 
-
     def to_dataframe(self, bnc2_list):
         '''
         Converts a list of benchmark results into a pandas DataFrame
@@ -330,7 +202,7 @@ class Runner:
                 if isinstance(v, np.ndarray):
                     b[k] = v.flatten()
                 if isinstance(v, list):
-                    b[k] = np.array(v)
+                    b[k] = np.array(v).flatten()
 
         # build data frame
         log.info('Build dataframe')
@@ -370,21 +242,64 @@ class Runner:
         self.save_dataframe(df)
 
         t_end = time.time()
-        log.info(f'Benchmark run in: {round((t_end - t_start) / 60, 1)} min')
+        log.info(f'Benchmark (serial) run in: {round((t_end - t_start) / 60, 1)} min')
+
+        return df
+
+    def run_benchmark_parallel(self, shapes=[], count=0, dir=None):
+        t_start = time.time()
+        # get data
+        data = []
+        if dir is str:            
+            data = self.load_instances(dir)
+        else:
+            data = self.build_instances(shapes, count)
+        
+        # run parallel
+        import solver_utils as solver
+        import joblib as job
+        def run(i, idx):
+            log.info(f'Instance #{idx}')
+            res = BenchmarkResult_v2(i)
+            log.info('Solve primal')
+            res = solver.solve_primal(i, res)
+            log.info('Solve relaxed')
+            res = solver.solve_relaxed(i, res)
+            log.info('Solve lagrange')
+            res = solver.solve_lagrangian(i, res)
+            return res
+
+        results = job.Parallel(n_jobs=-1, verbose=50)(job.delayed(run)(i,idx) for idx,i in enumerate(data))
+        
+
+        # save data file
+        self.save_instances(data)
+
+        # save result file
+        df = self.to_dataframe(results)
+        self.save_dataframe(df)
+
+        t_end = time.time()
+        log.info(f'Benchmark (parallel) run in: {round((t_end - t_start) / 60, 1)} min')
 
         return df
         
 
 
 if __name__ == '__main__':
-    c = [10,20,40,80,160]
-    l = [2,4,8,16]
-    shapes = np.array(np.meshgrid(c,l)).T.reshape(-1,2)    
-    repeat = 6    
+    c_long = [10,20,40,60,80]
+    l_long = [2,4,8,12,16]
+    s_long = np.array(np.meshgrid(c_long,l_long)).T.reshape(-1,2)    
+    r_long = 1    
     
+    c_short = [5,15,25]
+    l_short = [2,4,6,8]
+    s_short = np.array(np.meshgrid(c_short,l_short)).T.reshape(-1,2)    
+    r_short = 12 
+
     # run benchmark
     runner = Runner()
-    runner.run_benchmark(shapes,repeat)
+    runner.run_benchmark(s_short, r_short)
 
-
-
+    # run parallel
+    #runner.run_benchmark_parallel(s_short, r_short)
